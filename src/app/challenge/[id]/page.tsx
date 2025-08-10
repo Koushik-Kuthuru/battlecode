5db423c90858132583e9f3f551168139467902f8
Koushik <koushik0822@gmail.com>
2025-08-10 06:45:39 +0000

    not points undo that i meant the bar in the bottom where r=un code butto

--- src/app/challenge/[id]/page.tsx
+++ src/app/challenge/[id]/page.tsx
@@ -210,7 +210,7 @@ export default function ChallengeDetail() {
 
   return (
     <div className="h-full w-full flex flex-col bg-background">
-       <div className="flex-shrink-0 p-2 flex justify-between items-center border-b">
+       <div className="flex-shrink-0 p-2 flex justify-between items-center border-b bg-muted">
          <Select value={language} onValueChange={setLanguage}>
              <SelectTrigger className="w-[180px]">
                  <SelectValue placeholder="Select language" />
